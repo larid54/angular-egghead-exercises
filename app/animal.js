3b9f61e340e8889794653eaba9754b221417d72f
larid54 <gurgulpatryk7@gmail.com>
2015-07-31 13:11:56 +0200

    exercise 16 ang

--- app/animal.js
+++ app/animal.js
@@ -6,6 +6,7 @@ animalApp.directive('animal', function ()
 {
     return {
         restrict: 'E',
+        transclude:true,
         scope: {
             name: '@'
         },
@@ -13,9 +14,10 @@ animalApp.directive('animal', function ()
                 '<h3>{{name}}</h3>' +
                 '<div>' +
                 '<div ng-switch on="name">' +
-                '<div ng-switch-when="dog"><div class="btn btn-default"><img src="assets/dog.png"></div></div>' +
-                '<div ng-switch-when="cat"><div class="btn btn-default"><img src="assets/cat.png"></div></div>' +
-                '<div ng-switch-when="pig"><div class="btn btn-default"><img src="assets/pig.jpg"></div></div>' +
+                '<div ng-switch-when="dog"><div class="btn btn-default" ng-transclude><img src="assets/dog.png"></div></div>' +
+                '<div ng-switch-when="cat"><div class="btn btn-default"ng-transclude><img src="assets/cat.png"></div></div>' +
+                '<div ng-switch-when="pig"><div class="btn btn-default"ng-transclude><img src="assets/pig.jpg"></div></div>' +
                 '</div></div>'
     };
+    transclude: true
 });
